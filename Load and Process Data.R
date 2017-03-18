99e41409fa84589b117b4ea10683467bc6c73c78
Kyle Safran <ksafran356@gmail.com>
2017-03-18 10:28:54 -0400

    Changed names

---
+++ Load and Process Data.R
@@ -0,0 +1,27 @@
+# Load and Process Data
+
+library(jsonlite)
+library(dplyr)
+
+hop <- fromJSON('data/train.json')
+
+# Convert to a data.frame. Photos and features are both too long.
+photos <- hop$photos
+features <- hop$features
+
+hop$features <- NULL
+hop$photos <- NULL
+
+hop <- lapply(hop, unlist) %>% as.data.frame()
+hop$id <- rownames(hop)
+
+# Add Common Features
+library(tm)
+counts <- TermDocumentMatrix(Corpus(VectorSource(unlist(features))))
+common.features <- findFreqTerms(counts, 50)
+
+for(feat in common.features){
+  hop[[feat]] <- grepl(tolower(feat), tolower(features))
+}
+
+saveRDS(hop, 'data/hop_df.rds')
