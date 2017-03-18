b78ede80e6009841930eff26151607c1ab1b35bd
Kyle Safran <ksafran356@gmail.com>
2017-03-18 10:28:08 -0400

    fixed feature addition in data transformation

--- Load and EDA.R
+++ Load and EDA.R
@@ -12,18 +12,16 @@ features <- hop$features
 hop$features <- NULL
 hop$photos <- NULL
 
-hop2 <- lapply(hop, unlist) %>% as.data.frame()
-hop2$id <- rownames(hop2)
-# Add features as dummy variables
-unique.features <- unique(unlist(features))
-feature.extractor <- function(x){
-  x %in% features[[i]]
-}
+hop <- lapply(hop, unlist) %>% as.data.frame()
+hop$id <- rownames(hop)
 
+# Add Common Features
 library(tm)
 counts <- TermDocumentMatrix(Corpus(VectorSource(unlist(features))))
 common.features <- findFreqTerms(counts, 50)
 
-for(feat in unique.features){
-  hop2[[feat]] <- grepl(feat, features)
+for(feat in common.features){
+  hop[[feat]] <- grepl(tolower(feat), tolower(features))
 }
+
+saveRDS(hop, 'data/hop_df.rds')
