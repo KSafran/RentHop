4f7f538daad7c0d4896e82d2b6897488ba677de2
Kyle Safran <ksafran356@gmail.com>
2017-03-26 10:21:22 -0400

    added loss functions and an initial gbm.

---
+++ Modeling Functions.R
@@ -0,0 +1,28 @@
+### Modeling Functions
+
+
+logit <- function(x){
+  1/(1 + exp(-x))
+}
+
+reshape_predictions <- function(predictions, nclass){
+  matrix(predictions, ncol = nclass, byrow = TRUE)
+}
+
+reshape_actuals <- function(actuals){
+  cbind(actuals == 0, actuals == 1, actuals == 2)
+}
+
+competition_loss <- function(predictions, actuals){
+  scaled.preds <- rescale_probabilities(predictions)
+  shaped.actuals <- reshape_actuals(actuals)
+  -sum(shaped.actuals * scaled.preds)/nrow(shaped.actuals)
+}
+
+rescale_probabilities <- function(x){
+  scaled.x <- x/rowSums(x)  
+  pmax(pmin(scaled.x, (1 - 1e-15)), 1e-15) %>% 
+    log()
+}
+
+
---
+++ gbm.R
@@ -0,0 +1,50 @@
+### First GBM
+
+# Loading the Training Data
+# Right now we can just source this utils script, but we should 
+# wrap this in a function something like 
+# source(utils.R)
+# load_libraries()
+# hop <- load_training_data()
+source('utils.R')
+
+# Set up GBM
+train.indicator <- runif(nrow(hop)) < 0.7
+
+# Set Predictors
+predictors <- hop %>% 
+  select(bathrooms, bedrooms, latitude, longitude, price,
+         two_br:n.photos) %>% colnames()
+
+model.formula <- as.formula(paste0('class ~ ',
+                                   paste(predictors, collapse = ' + '),
+                                   ' - 1')) # removes intercept
+
+train.matrix <- model.matrix(model.formula, data = hop[train.indicator,])
+train.xgbd.matrix <- xgb.DMatrix(train.matrix, label = hop$class[train.indicator])
+test.matrix <- model.matrix(model.formula, data = hop[!train.indicator,])
+test.xgbd.matrix <- xgb.DMatrix(test.matrix, label = hop$class[!train.indicator])
+watchlist <- list(train = train.xgbd.matrix, eval = test.xgbd.matrix)
+
+gbm.parameters <- list(eta = 0.1,
+                       max.depth = 5,
+                       min_child_weight = 10,
+                       subsample = 0.8,
+                       colsample_bytree = 0.8,
+                       lambda = 1,
+                       objective = 'multi:softprob',
+                       num_class = 3)
+
+gbm.model <- xgb.train(params = gbm.parameters,
+                       data = train.xgbd.matrix,
+                       watchlist = watchlist,
+                       nrounds = 1000,
+                       print_every_n = 10,
+                       early_stopping_rounds = 25)
+
+test.predictions <- predict(gbm.model, newdata = test.xgbd.matrix,
+                            ntreelimit = 226) %>% 
+  reshape_predictions(3)
+
+competition_loss(predictions = test.predictions, 
+                 actuals = hop$class[!train.indicator])
